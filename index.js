c5d362abcac6411cacb84493add0e58ad38cad7d
ThiagoGama1 <160535848+ThiagoGama1@users.noreply.github.com>
2024-05-22 10:43:42 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -10,15 +10,40 @@
 
 
 
+function tabela3(nome){
 
-function tabela(){
+    //elementos principais
+    const tabela1 = document.createElement("table");
+    const head = document.createElement("thead");
+    const tabela4 = document.createElement("tbody");
+
+    //criando thead
+
+    const nome = prompt("Qual é o seu nome?");
+    
+
+
+
+    //criando elementos tbody
+
+    const th1 = document.createElement("th");
+    const th2 = document.createElement("th");
+    const th3 = document.createElement("th");
+    const th4 = document.createElement("th");
+    const th5 = document.createElement("th");
+    th1.innerHTML("B");
+    th2.innerHTML("I");
+    th3.innerHTML("N");
+    th4.innerHTML("G");
+    th5.innerHTML("O");
+
     for(let i=0; i < 5; i++)
     {
         const tr = document.createElement("tr");
         for(let c = 0; c < 5; c++)
         {
             const td = document.createElement("td");
-            td.innerHTML(XX);
+            td.innerHTML("XX");
             tr.appendChild(td);
         }
     }
